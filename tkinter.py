4825c3e1b53e48ed17399aa55d431ffaeb6db415
Denshell Denejour <121324587+DenshellDenejour@users.noreply.github.com>
2023-01-28 21:43:49 -0400

    Create tkinter.py

---
+++ tkinter.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+
+root=tk.Tk()
+w= root.winfo_height
+root.geometry("400x400+500+400")
+
+lab1 =tk.Label(root,
+      text="Denshell Denejour",
+      font="Arial 20",
+      bg="yellow")
+   lab1.pack()
+   
+   root.mainloop()
+
